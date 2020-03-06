3f5807ded67eacb75a0237802d3a1c0cd92ec8d5
Matthew Linkous <matthew.linkous@gmail.com>
2020-03-06 13:23:39 -0800

    Export AspenDB directly as well as default locally bound verion

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import PouchDB from "pouchdb";
 import AspenDB from "./aspendb";
+import { DefaultDeserializer } from "v8";
 
 PouchDB.plugin(require("pouchdb-adapter-node-websql"));
 
-export default AspenDB.bind(
+const LocalAspenDB = AspenDB.bind(
   null,
   new PouchDB(AspenDB.localDBPath, { adapter: "websql" }),
 );
+
+export { LocalAspenDB as default, AspenDB };
