d196d7344435784f6c5d68b72a41da75227a450b
Matthew Linkous <matthew.linkous@gmail.com>
2020-03-21 09:59:34 -0700

    Fix constant casing

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,11 @@ PouchDB.plugin(require("pouchdb-adapter-node-websql"));
 
 const { getDataHome } = require("platform-folders");
 const appDirectory = `${getDataHome()}/aspen/`;
-const localDBPath = appDirectory + "aspen_local.db";
+const LOCAL_DB_PATH = appDirectory + "aspen_local.db";
 
 const LocalAspenDB = AspenDB.bind(
   null,
-  new PouchDB(localDBPath, { adapter: "websql" }),
+  new PouchDB(LOCAL_DB_PATH, { adapter: "websql" }),
 );
 
 export { LocalAspenDB as default, AspenDB, AspenAppScope };
